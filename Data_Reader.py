bbefd0bf263ba54fbc90211de6e2dd8d50cbc37f
Priyanshi Jain <priyanshi.jain030@gmail.com>
2020-10-28 21:26:44 +0530

    Update Data_Reader.py

--- Data_Reader.py
+++ Data_Reader.py
@@ -2,11 +2,10 @@ import numpy as np
 import os
 import scipy.misc as misc
 import random
-import imageio
 import sklearn
+import cv2
 import PIL
 from PIL import Image
-import cv2
 #------------------------Class for reading training and  validation data---------------------------------------------------------------------
 class Data_Reader:
 
@@ -66,108 +65,104 @@ class Data_Reader:
                       if(self.ReadLabels):
                         self.SLabels.append(self.LabelFiles[Lb[i] + k])
 ###########################Read and augment next batch of images and labels#####################################################################################
-    def ReadAndAugmentNextBatch(self):
-        if self.itr>=self.NumFiles: # End of an epoch
-            self.itr=0
-            self.SuffleBatch()
-            self.Epoch+=1
-        batch_size=np.min([self.BatchSize,self.NumFiles-self.itr])
-        Sy =Sx= 0
-        XF=YF=1
-        Cry=1
-        Crx=1
-#--------------Resize Factor--------------------------------------------------------
-        if np.random.rand() < 1:
-            YF = XF = 0.3+np.random.rand()*0.7
-#------------Stretch image-------------------------------------------------------------------
-        if np.random.rand()<0.8:
-            if np.random.rand()<0.5:
-                XF*=0.5+np.random.rand()*0.5
-            else:
-                YF*=0.5+np.random.rand()*0.5
-#-----------Crop Image------------------------------------------------------
-        if np.random.rand()<0.0:
-            Cry=0.7+np.random.rand()*0.3
-            Crx = 0.7 + np.random.rand() * 0.3
+#     def ReadAndAugmentNextBatch(self):
+#         if self.itr>=self.NumFiles: # End of an epoch
+#             self.itr=0
+#             self.SuffleBatch()
+#             self.Epoch+=1
+#         batch_size=np.min([self.BatchSize,self.NumFiles-self.itr])
+#         Sy =Sx= 0
+#         XF=YF=1
+#         Cry=1
+#         Crx=1
+# #--------------Resize Factor--------------------------------------------------------
+# #         if np.random.rand() < 1:
+# #             YF = XF = 0.3+np.random.rand()*0.7
+# # #------------Stretch image-------------------------------------------------------------------
+# #         if np.random.rand()<0.8:
+# #             if np.random.rand()<0.5:
+# #                 XF*=0.5+np.random.rand()*0.5
+# #             else:
+# #                 YF*=0.5+np.random.rand()*0.5
+# # #-----------Crop Image------------------------------------------------------
+# #         if np.random.rand()<0.0:
+# #             Cry=0.7+np.random.rand()*0.3
+# #             Crx = 0.7 + np.random.rand() * 0.3
 
-#-----------Augument Images and labeles-------------------------------------------------------------------
+# #-----------Augument Images and labeles-------------------------------------------------------------------
 
-        for f in range(batch_size):
+#         for f in range(batch_size):
 
-#.............Read image and labels from files.........................................................
-          Img = np.array(imageio.imread(self.Image_Dir + self.SFiles[self.itr]))
-          if(Img.ndim < 3):
-            continue
-          Img=Img[:,:,0:3]
-          # LabelName=self.SFiles[self.itr][0:-4]+".png"# Assume Label name is same as image only with png ending
-          if self.ReadLabels:
-              Label= imageio.imread(self.Label_Dir + self.SLabels[self.itr])
-              Label = Label/255.0
-          self.itr+=1
-#............Set Batch image size according to first image in the batch...................................................
-          if f==0:
-                Sy, Sx,d = Img.shape
-                Sy,Sx
-                Sy*=YF
-                Sx*=XF
-                Cry*=Sy
-                Crx*=Sx
-                Sy = np.int32(Sy)
-                Sx = np.int32(Sx)
-                Cry = np.int32(Cry)
-                Crx = np.int32(Crx)
-                Images = np.zeros([batch_size,Cry,Crx,3], dtype=np.float)
-                if self.ReadLabels: Labels= np.zeros([batch_size,Cry,Crx,1], dtype=np.int)
+# #.............Read image and labels from files.........................................................
+#           Img = np.array(imageio.imread(self.Image_Dir + self.SFiles[self.itr]))
+#           if(Img.ndim < 3):
+#             Img = cv2.cvtColor(Img,cv2.COLOR_GRAY2RGB)
+#           Img=Img[:,:,0:3]
+#           # LabelName=self.SFiles[self.itr][0:-4]+".png"# Assume Label name is same as image only with png ending
+#           if self.ReadLabels:
+#               Label= imageio.imread(self.Label_Dir + self.SLabels[self.itr])
+#               Label = Label/255.0
+#           self.itr+=1
+# #............Set Batch image size according to first image in the batch...................................................
+#           if f==0:
+#                 Sy, Sx,d = Img.shape
+#                 Cry =Sy
+#                 Crx =Sx
+#                 Sy = np.int32(Sy)
+#                 Sx = np.int32(Sx)
+#                 Cry = np.int32(Cry)
+#                 Crx = np.int32(Crx)
+#                 Images = np.zeros([batch_size,Cry,Crx,3], dtype=np.float)
+#                 if self.ReadLabels: Labels= np.zeros([batch_size,Cry,Crx,1], dtype=np.int)
 
 
-#..........Resize and strecth image and labels....................................................................
-          #  Img = misc.imresize(Img, [Sy,Sx], interp='bilinear')
-          Img = np.array(Image.fromarray(Img).resize([Sx,Sy], Image.BILINEAR))
-          if self.ReadLabels: Label= np.array(Image.fromarray(Label).resize([Sx,Sy], Image.NEAREST))
+# #..........Resize and strecth image and labels....................................................................
+#           #  Img = misc.imresize(Img, [Sy,Sx], interp='bilinear')
+#           Img = np.array(Image.fromarray(Img).resize([Sx,Sy], Image.BILINEAR))
+#           if self.ReadLabels: Label= np.array(Image.fromarray(Label).resize([Sx,Sy], Image.NEAREST))
 
-#-------------------------------Crop Image.......................................................................
-          MinOccupancy=501
-          if not (Cry==Sy and Crx==Sx):
-              for u in range(501):
-                  MinOccupancy-=1
-                  Xi=np.int32(np.floor(np.random.rand()*(Sx-Crx)))
-                  Yi=np.int32(np.floor(np.random.rand()*(Sy-Cry)))
-                  if np.sum(Label[Yi:Yi+Cry,Xi:Xi+Crx]>0)>MinOccupancy:
-                      Img=Img[Yi:Yi+Cry,Xi:Xi+Crx,:]
-                      if self.ReadLabels: Label=Label[Yi:Yi+Cry,Xi:Xi+Crx]
-                      break
-#------------------------Mirror Image-------------------------------# --------------------------------------------
-          if random.random()<0.5: # Agument the image by mirror image
-              Img=np.fliplr(Img)
-              if self.ReadLabels:
-                  Label=np.fliplr(Label)
+# #-------------------------------Crop Image.......................................................................
+# #           MinOccupancy=501
+# #           if not (Cry==Sy and Crx==Sx):
+# #               for u in range(501):
+# #                   MinOccupancy-=1
+# #                   Xi=np.int32(np.floor(np.random.rand()*(Sx-Crx)))
+# #                   Yi=np.int32(np.floor(np.random.rand()*(Sy-Cry)))
+# #                   if np.sum(Label[Yi:Yi+Cry,Xi:Xi+Crx]>0)>MinOccupancy:
+# #                       Img=Img[Yi:Yi+Cry,Xi:Xi+Crx,:]
+# #                       if self.ReadLabels: Label=Label[Yi:Yi+Cry,Xi:Xi+Crx]
+# #                       break
+# # #------------------------Mirror Image-------------------------------# --------------------------------------------
+# #           if random.random()<0.5: # Agument the image by mirror image
+# #               Img=np.fliplr(Img)
+# #               if self.ReadLabels:
+# #                   Label=np.fliplr(Label)
 
-#-----------------------Agument color of Image-----------------------------------------------------------------------
-          Img = np.float32(Img)
-          if np.random.rand() < 0.8:  # Play with shade
-              Img *= 0.4 + np.random.rand() * 0.6
-          if np.random.rand() < 0.4:  # Turn to grey
-              Img[:, :, 2] = Img[:, :, 1]=Img[:, :, 0] = Img[:,:,0]=Img.mean(axis=2)
+# # #-----------------------Agument color of Image-----------------------------------------------------------------------
+# #           Img = np.float32(Img)
+# #           if np.random.rand() < 0.8:  # Play with shade
+# #               Img *= 0.4 + np.random.rand() * 0.6
+# #           if np.random.rand() < 0.4:  # Turn to grey
+# #               Img[:, :, 2] = Img[:, :, 1]=Img[:, :, 0] = Img[:,:,0]=Img.mean(axis=2)
 
-          if np.random.rand() < 0.0:  # Play with color
-              if np.random.rand() < 0.6:
-                for i in range(3):
-                    Img[:, :, i] *= 0.1 + np.random.rand()
-              if np.random.rand() < 0.2:  # Add Noise
-                  Img *=np.ones(Img.shape)*0.95 + np.random.rand(Img.shape[0],Img.shape[1],Img.shape[2])*0.1
-          Img[Img>255]=255
-          Img[Img<0]=0
-#----------------------Add images and labels to to the batch----------------------------------------------------------
-          Img = Img/255.0
-          Images[f]=Img
-          if self.ReadLabels:
-                Labels[f,:,:,0]=Label
+# #           if np.random.rand() < 0.0:  # Play with color
+# #               if np.random.rand() < 0.6:
+# #                 for i in range(3):
+# #                     Img[:, :, i] *= 0.1 + np.random.rand()
+# #               if np.random.rand() < 0.2:  # Add Noise
+# #                   Img *=np.ones(Img.shape)*0.95 + np.random.rand(Img.shape[0],Img.shape[1],Img.shape[2])*0.1
+# #           Img[Img>255]=255
+# #           Img[Img<0]=0
+# #----------------------Add images and labels to to the batch----------------------------------------------------------
+#           Images[f]=Img
+#           if self.ReadLabels:
+#                 Labels[f,:,:,0]=Label
 
-#.......................Return aumented images and labels...........................................................
-        if self.ReadLabels:
-            return Images, Labels# return image and pixelwise labels
-        else:
-            return Images# Return image
+# #.......................Return aumented images and labels...........................................................
+#         if self.ReadLabels:
+#             return Images, Labels# return image and pixelwise labels
+#         else:
+#             return Images# Return image
 
 
 
@@ -182,14 +177,14 @@ class Data_Reader:
 
         for f in range(batch_size):
 ##.............Read image and labels from files.........................................................
-           Img = imageio.imread(self.Image_Dir + self.OrderedFiles[self.itr])
+           Img = cv2.imread(self.Image_Dir + self.OrderedFiles[self.itr])
            if(Img.ndim<3):
              Img = cv2.cvtColor(Img,cv2.COLOR_GRAY2RGB)
            Img=Img[:,:,0:3]
+           Img = cv2.cvtColor(Img, cv2.COLOR_BGR2RGB)
           #  LabelName=self.OrderedFiles[self.itr][0:-4]+".png"# Assume label name is same as image only with png ending
            if self.ReadLabels:
-              Label= imageio.imread(self.Label_Dir + self.LabelFiles[self.itr])
-              Label = Label/255.0
+              Label= cv2.imread(self.Label_Dir + self.LabelFiles[self.itr],0)              
            self.itr+=1
 #............Set Batch size according to first image...................................................
            if f==0:
@@ -199,9 +194,13 @@ class Data_Reader:
 
 #..........Resize image and labels....................................................................
            Img = np.array(Image.fromarray(Img).resize([Sx,Sy], Image.BILINEAR))
-           if self.ReadLabels: Label = np.array(Image.fromarray(Label).resize([Sx,Sy], Image.NEAREST))
+           if self.ReadLabels: 
+            Label = np.array(Image.fromarray(Label).resize([Sx,Sy], Image.NEAREST))
+            Label = Label/255.0
+
 #...................Load image and label to batch..................................................................
            Images[f] = Img
+           
            if self.ReadLabels:
               Labels[f, :, :, 0] = Label
 #...................................Return images and labels........................................
